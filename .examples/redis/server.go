5d8ecb9726ff1b88a44b1570fb40c0b24ed89d36
ClownFish <osgochina@gmail.com>
2021-12-16 18:49:37 +0800

    有限的支持redis协议

--- .examples/redis/server.go
+++ .examples/redis/server.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"github.com/gogf/gf/container/gmap"
+	"github.com/gogf/gf/util/gconv"
 	"github.com/osgochina/dmicro/drpc"
 	"github.com/osgochina/dmicro/drpc/codec/redis_codec"
 	"github.com/osgochina/dmicro/drpc/plugin/redis"
 	"github.com/osgochina/dmicro/drpc/proto/redisproto"
+	"github.com/osgochina/dmicro/utils/graceful"
 )
+
+var db *gmap.AnyAnyMap
 
 func main() {
 	//开启信号监听
-	go drpc.GraceSignal()
-
+	go graceful.GraceSignal()
+	db = gmap.New(true)
 	svr := drpc.NewEndpoint(drpc.EndpointConfig{
 		CountTime:   true,
 		LocalIP:     "127.0.0.1",
@@ -28,7 +33,25 @@ type Redis struct {
 }
 
 func (m *Redis) Get(arg *redis_codec.CmdLine) (redis_codec.Msg, *drpc.Status) {
-
+	if len(*arg) > 0 {
+		key := gconv.String((*arg)[0])
+		val := db.GetVar(key)
+		if val.IsNil() {
+			return redis_codec.MakeNullBulkMsg(), nil
+		}
+		return redis_codec.MakeBulkMsg(val.Bytes()), nil
+	}
 	// response
-	return redis_codec.MakeBulkMsg([]byte("liuizhiming")), nil
+	return redis_codec.MakeErrorMsg("参数错误"), nil
+}
+
+func (m *Redis) Set(arg *redis_codec.CmdLine) (redis_codec.Msg, *drpc.Status) {
+	if len(*arg) == 2 {
+		key := gconv.String((*arg)[0])
+		val := gconv.String((*arg)[1])
+		db.Set(key, val)
+		return redis_codec.MakeSuccessMsg("ok"), nil
+	}
+	// response
+	return redis_codec.MakeErrorMsg("参数错误"), nil
 }
