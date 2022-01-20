8ec16f20756d85d79f037ba4fd1902871fbb28fb
ClownFish <osgochina@gmail.com>
2022-01-20 13:23:39 +0800

    增加编译版本输出

--- easyservice/build.go
+++ easyservice/build.go
@@ -13,16 +13,24 @@ var (
 	BuildGitCommitId = "No Commit Info"
 	BuildTime        = "No Time Info"
 	Authors          = "No Authors Info"
+	Logo             = `
+  ____    __  __   _                       
+ |  _ \  |  \/  | (_)   ___   _ __    ___  
+ | | | | | |\/| | | |  / __| | '__|  / _ \ 
+ | |_| | | |  | | | | | (__  | |    | (_) |
+ |____/  |_|  |_| |_|  \___| |_|     \___/
+`
 )
 
 //显示版本信息
 func (that *EasyService) version() {
-	fmt.Printf("Server Version: %s\n", BuildVersion)
-	fmt.Printf("Server Build Time: %s\n", BuildTime)
-	fmt.Printf("Go version: %s\n", BuildGoVersion)
-	fmt.Printf("Git commit: %s\n", BuildGitCommitId)
-	fmt.Printf("DMicro Version: %s\n", dmicro.Version)
-	fmt.Printf("GF Version: %s\n", gf.VERSION)
-	fmt.Printf("Authors: %s\n", Authors)
-	fmt.Printf("Install Path: %s\n", gfile.SelfPath())
+	fmt.Println(Logo)
+	fmt.Printf("Version:         %s\n", BuildVersion)
+	fmt.Printf("Go Version:      %s\n", BuildGoVersion)
+	fmt.Printf("DMicro Version:  %s\n", dmicro.Version)
+	fmt.Printf("GF Version:      %s\n", gf.VERSION)
+	fmt.Printf("Git Commit:      %s\n", BuildGitCommitId)
+	fmt.Printf("Build Time:      %s\n", BuildTime)
+	fmt.Printf("Authors:         %s\n", Authors)
+	fmt.Printf("Install Path:    %s\n", gfile.SelfPath())
 }
