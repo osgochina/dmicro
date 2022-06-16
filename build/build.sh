6148ec98761da6d2b59ab6aec12c99329597e681
ClownFish <osgochina@gmail.com>
2022-06-16 15:40:00 +0800

    编译脚本编译的时候默认去除符号表

--- build/build.sh
+++ build/build.sh
@@ -100,10 +100,10 @@ ldflags+=("-X" "\"github.com/osgochina/dmicro/easyservice.BuildGitCommitId=${bui
 ldflags+=("-X" "\"github.com/osgochina/dmicro/easyservice.BuildTime=${build_date}\"")
 
 # -s -w 去除符号表
-#${goBin} build -v -ldflags "${ldflags[*]} -s -w"  -o "${output_file}" $build_file || exit 1
+${goBin} build -v -ldflags "${ldflags[*]} -s -w"  -o "${output_file}" $build_file || exit 1
 # 加壳
 #upx "${output_file}" -o "${output_file}".upx
 
-${goBin} build -v -ldflags "${ldflags[*]}"  -o "${output_file}" oa || exit 1
+#${goBin} build -v -ldflags "${ldflags[*]}"  -o "${output_file}" oa || exit 1
 echo "build linux amd64 done."
 
